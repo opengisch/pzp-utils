06f767af0644366f21842ceb9018559ac0c6d8e0
Mario Baranzini <mario@opengis.ch>
2022-10-18 14:24:20 +0200

    Add simplify intensity algorithm

--- pzp_utils/processing/provider.py
+++ pzp_utils/processing/provider.py
@@ -2,12 +2,14 @@ from qgis.core import QgsProcessingProvider
 
 from pzp_utils.processing.apply_matrix import ApplyMatrix
 from pzp_utils.processing.danger_zones import DangerZones
+from pzp_utils.processing.simplify_intensity import SimplifyIntensity
 
 
 class Provider(QgsProcessingProvider):
     def loadAlgorithms(self, *args, **kwargs):
         self.addAlgorithm(DangerZones())
         self.addAlgorithm(ApplyMatrix())
+        self.addAlgorithm(SimplifyIntensity())
 
     def id(self, *args, **kwargs):
         return "pzp"
