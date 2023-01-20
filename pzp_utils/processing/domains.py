1ebf11821ad9c3ddce489c186b7bfb70f02f64c8
Mario Baranzini <mario@opengis.ch>
2023-01-20 14:25:14 +0100

    Move domains there from pzp plugin

--- pzp_utils/processing/domains.py
+++ pzp_utils/processing/domains.py
@@ -1,5 +1,59 @@
 from collections import OrderedDict
 
+PROPAGATION_PROBABILITIES = OrderedDict(
+    {
+        1001: "Bassa",
+        1002: "Media",
+        1003: "Alta",
+    }
+)
+
+EVENT_PROBABILITIES = OrderedDict(
+    {
+        1000: "Molto bassa",
+        1001: "Bassa",
+        1002: "Media",
+        1003: "Alta",
+    }
+)
+
+
+INTENSITIES = OrderedDict(
+    {
+        1000: "Nessun impatto",
+        1001: "Impatto presente",
+        1002: "Debole",
+        1003: "Medio",
+        1004: "Forte",
+    }
+)
+
+DANGER_TYPES = OrderedDict(
+    {
+        1000: "Non in pericolo",
+        1001: "Pericolo residuo",  # Light yellow
+        1002: "Basso",  # Yellow
+        1003: "Medio",  # Blue
+        1004: "Elevato",  # Red
+    }
+)
+
+DANGER_LEVELS = OrderedDict(
+    {
+        1000: 0,
+        1001: 9,
+        1002: 8,
+        1003: 7,
+        1004: 6,
+        1005: 5,
+        1006: 4,
+        1007: 3,
+        1008: 2,
+        1009: 1,
+        1010: -10,
+    }
+)
+
 PROCESS_TYPES = OrderedDict(
     {
         1110: "Alluvionamento corso d'acqua minore",
